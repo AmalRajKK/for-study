d05ad094e18e8e7cc10870c5ed56945bde3e7256
Akashkv15 <akashkv1931@gmail.com>
2024-08-11 12:54:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function cl(value){
+    document.getElementById("screen").value+=value
+}
+function clearscreen(){
+    document.getElementById("screen").value=""
+}
+function equalclick(){
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
